4b53159bc5544466949ae60f9594fcb76ad291fb
kabhishek28 <kabhishek.eng@gmail.com>
2025-04-06 09:43:21 +0530

    Full-Stack CRUD Operation Application

---
+++ fullstack-backend/src/main/java/com/kabhi/fullstack_backend/controller/userController.java
@@ -0,0 +1,56 @@
+package com.kabhi.fullstack_backend.controller;
+
+import com.kabhi.fullstack_backend.exception.UserNotFoundException;
+import com.kabhi.fullstack_backend.model.User;
+import com.kabhi.fullstack_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+public class userController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/user")
+    User newUser(@RequestBody User newUser){
+        return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+
+    @GetMapping("/user/{id}")
+    User getUserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUserName(newUser.getUserName());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id "+id+"has been deleted success.";
+    }
+
+}
---
+++ fullstack-backend/src/main/java/com/kabhi/fullstack_backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kabhi.fullstack_backend.repository;
+
+import com.kabhi.fullstack_backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+}
